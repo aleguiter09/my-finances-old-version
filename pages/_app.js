76dc7714488dddb9db11b91462963aa530a287ab
aguiter <aguiter@folderit.net>
2022-11-25 12:18:56 -0300

    Icon0

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
+      <Head>
+        <link rel="shortcut  icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/static/apple-touch-icon.png" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
