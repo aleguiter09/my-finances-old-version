7d22fe170cc66ff2bb4b642f30bca07ca765aa29
aguiter <aguiter@folderit.net>
2022-12-02 09:19:57 -0300

    Added Modals and alerts

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,13 +1,59 @@
-import { Card, CardBody, Stat, StatLabel, StatNumber } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Button,
+  Flex,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
+import { AlertComponent } from "./AlertComponent";
 
-export const CardComponent = ({ title, amount }) => {
+export const CardComponent = ({ title, amount, handleDelete }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const onClickDelete = () => {
+    handleDelete(title);
+    onClose();
+  };
+
   return (
-    <Card minWidth="150" mx="2">
-      <CardBody>
-        <Stat>
-          <StatLabel>{title}</StatLabel>
-          <StatNumber fontSize="xl">$ {amount.toFixed(1)}</StatNumber>
-        </Stat>
+    <>
+      <AlertComponent
+        isOpen={isOpen}
+        onClose={onClose}
+        onClickDelete={onClickDelete}
+      />
+      <Card minWidth="150" mx="2">
+        <Flex justifyContent="end">
+          <DeleteIcon
+            fontSize="xs"
+            mt="2"
+            me="2"
+            cursor="pointer"
+            onClick={onOpen}
+          />
+        </Flex>
+        <CardBody pt="0">
+          <Stat>
+            <StatLabel>{title}</StatLabel>
+            <StatNumber fontSize="xl">$ {amount.toFixed(1)}</StatNumber>
+          </Stat>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export const CardButtonComponent = ({ handleClick }) => {
+  return (
+    <Card minWidth="120" mx="2">
+      <CardBody align="center" mt="1">
+        <Button onClick={handleClick} rounded="25">
+          <AddIcon />
+        </Button>
       </CardBody>
     </Card>
   );
--- pages/home/[id].js
+++ pages/home/[id].js
@@ -1,11 +1,26 @@
 import { useRouter } from "next/router";
-import { Flex, Button, Heading } from "@chakra-ui/react";
-import { CardComponent } from "../../components/CardComponent";
+import { Flex, Button, Heading, Text, useDisclosure } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import {
+  CardComponent,
+  CardButtonComponent,
+} from "../../components/CardComponent";
+import { ModalAddAccount } from "../../components/ModalAddAccount";
+import { ModalAddExpense } from "../../components/ModalAddExpense";
 import { useEffect, useState } from "react";
 
 const Home = () => {
   const router = useRouter();
+  const {
+    isOpen: isAddAccountOpen,
+    onOpen: onAddAccountOpen,
+    onClose: onAddAccountClose,
+  } = useDisclosure();
+  const {
+    isOpen: isAddExpenseOpen,
+    onOpen: onAddExpenseOpen,
+    onClose: onAddExpenseClose,
+  } = useDisclosure();
 
   const accountsDefault = [
     { title: "Santander", amount: 30000 },
@@ -16,6 +31,7 @@ const Home = () => {
 
   const [total, setTotal] = useState(0);
   const [accounts, setAccounts] = useState(accountsDefault);
+  const [newAccount, setNewAccount] = useState({});
 
   useEffect(() => {
     let sum = 0;
@@ -28,23 +44,60 @@ const Home = () => {
     router.push("/");
   };
 
-  const handleClick = (e) => {
-    e.preventDefault();
+  const handleAddAccount = (newAccount = {}) => {
+    if (newAccount.title && newAccount.title !== "") {
+      setAccounts([...accounts, newAccount]);
+      setNewAccount({});
+      onAddAccountClose();
+    }
+  };
+
+  const handleAddExpense = () => {
+    /*if (newAccount.title && newAccount.title !== "") {
+      setAccounts([...accounts, newAccount]);
+      setNewAccount({});
+      
+    }*/
+    console.log("Hola");
+    onAddExpenseClose();
+  };
+
+  const handleDelete = (title) => {
+    setAccounts(accounts.filter((a) => a.title !== title));
   };
 
   return (
     <Flex direction="column" height="100vh" mt="4">
-      <Heading size="lg" alignSelf="self-start" ms="2">
-        Total: $ {total.toFixed(1)}
+      <ModalAddAccount
+        isOpen={isAddExpenseOpen}
+        onClose={onAddExpenseClose}
+        onConfirm={handleAddExpense}
+        setNewAccount={null}
+      />
+      <ModalAddExpense
+        isOpen={isAddAccountOpen}
+        onClose={onAddAccountClose}
+        onConfirm={handleAddAccount}
+        setNewAccount={setNewAccount}
+      />
+      <Heading size="2xl" alignSelf="center" ms="2">
+        $ {total.toFixed(1)}
       </Heading>
-      <Flex my="4" maxWidth="100%" overflowX="auto">
+      <Flex my="4" overflowX="auto">
         {accounts.map((acc) => (
-          <CardComponent title={acc.title} amount={acc.amount} />
+          <CardComponent
+            key={acc.title}
+            title={acc.title}
+            amount={acc.amount}
+            handleDelete={handleDelete}
+          />
         ))}
+        <CardButtonComponent handleClick={onAddAccountOpen} />
       </Flex>
       <Flex direction="column" align="center">
-        <Button mb="3" onClick={handleClick} rounded="25">
-          <AddIcon me="3" /> Add account
+        <Button colorScheme="blue" mb="3" onClick={onAddExpenseOpen}>
+          <AddIcon me="2" />
+          <Text>Add</Text>
         </Button>
         <Button onClick={handleBack}>Go back</Button>
       </Flex>
