b631f81acd7cde7d5d2bf3e9be96ba3cf3b06cce
aguiter <aguiter@folderit.net>
2022-12-02 16:55:07 -0300

    Added drawer expenses

--- pages/home/[id].js
+++ pages/home/[id].js
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
-import { Flex, Button, Heading, Text, useDisclosure } from "@chakra-ui/react";
-import { AddIcon } from "@chakra-ui/icons";
+import { Flex, Button, Heading, useDisclosure } from "@chakra-ui/react";
+import { AddIcon, MinusIcon } from "@chakra-ui/icons";
 import {
   CardComponent,
   CardButtonComponent,
 } from "../../components/CardComponent";
 import { ModalAddAccount } from "../../components/ModalAddAccount";
-import { ModalAddExpense } from "../../components/ModalAddExpense";
+import { DrawerAddFinance } from "../../components/DrawerAddFinance";
 import { useEffect, useState } from "react";
 
 const Home = () => {
@@ -16,10 +16,23 @@ const Home = () => {
     onOpen: onAddAccountOpen,
     onClose: onAddAccountClose,
   } = useDisclosure();
+
   const {
     isOpen: isAddExpenseOpen,
     onOpen: onAddExpenseOpen,
     onClose: onAddExpenseClose,
+  } = useDisclosure();
+
+  const {
+    isOpen: isAddIncomeOpen,
+    onOpen: onAddIncomeOpen,
+    onClose: onAddIncomeClose,
+  } = useDisclosure();
+
+  const {
+    isOpen: isAddCreditOpen,
+    onOpen: onAddCreditOpen,
+    onClose: onAddCreditClose,
   } = useDisclosure();
 
   const accountsDefault = [
@@ -29,9 +42,15 @@ const Home = () => {
     { title: "Belo", amount: 0 },
   ];
 
+  const categoriesDefault = ["Food", "House", "Cat"];
+  const categoriesDefaultWithValues = [
+    { title: "Food", amount: 30000 },
+    { title: "House Pago", amount: 12000 },
+    { title: "Cat", amount: 0 },
+  ];
+
   const [total, setTotal] = useState(0);
   const [accounts, setAccounts] = useState(accountsDefault);
-  const [newAccount, setNewAccount] = useState({});
 
   useEffect(() => {
     let sum = 0;
@@ -47,19 +66,28 @@ const Home = () => {
   const handleAddAccount = (newAccount = {}) => {
     if (newAccount.title && newAccount.title !== "") {
       setAccounts([...accounts, newAccount]);
-      setNewAccount({});
       onAddAccountClose();
     }
   };
 
-  const handleAddExpense = () => {
-    /*if (newAccount.title && newAccount.title !== "") {
-      setAccounts([...accounts, newAccount]);
-      setNewAccount({});
-      
-    }*/
-    console.log("Hola");
+  const handleAddExpense = (updatedAccount = {}) => {
+    accounts.map((acc) => {
+      if (acc.title === updatedAccount.title) {
+        acc.amount -= updatedAccount.amount;
+      }
+    });
+    setAccounts([...accounts]);
     onAddExpenseClose();
+  };
+
+  const handleAddIncome = (updatedAccount = {}) => {
+    accounts.map((acc) => {
+      if (acc.title === updatedAccount.title) {
+        acc.amount += updatedAccount.amount;
+      }
+    });
+    setAccounts([...accounts]);
+    onAddIncomeClose();
   };
 
   const handleDelete = (title) => {
@@ -69,16 +97,25 @@ const Home = () => {
   return (
     <Flex direction="column" height="100vh" mt="4">
       <ModalAddAccount
-        isOpen={isAddExpenseOpen}
-        onClose={onAddExpenseClose}
-        onConfirm={handleAddExpense}
-        setNewAccount={null}
-      />
-      <ModalAddExpense
         isOpen={isAddAccountOpen}
         onClose={onAddAccountClose}
         onConfirm={handleAddAccount}
-        setNewAccount={setNewAccount}
+      />
+      <DrawerAddFinance
+        isOpen={isAddExpenseOpen}
+        onClose={onAddExpenseClose}
+        onConfirm={handleAddExpense}
+        title={"Expense"}
+        accounts={accounts}
+        categories={categoriesDefault}
+      />
+      <DrawerAddFinance
+        isOpen={isAddIncomeOpen}
+        onClose={onAddIncomeClose}
+        onConfirm={handleAddIncome}
+        title={"Income"}
+        accounts={accounts}
+        categories={categoriesDefault}
       />
       <Heading size="2xl" alignSelf="center" ms="2">
         $ {total.toFixed(1)}
@@ -95,11 +132,30 @@ const Home = () => {
         <CardButtonComponent handleClick={onAddAccountOpen} />
       </Flex>
       <Flex direction="column" align="center">
-        <Button colorScheme="blue" mb="3" onClick={onAddExpenseOpen}>
-          <AddIcon me="2" />
-          <Text>Add</Text>
+        <Flex>
+          <Button
+            minWidth="160"
+            colorScheme="blue"
+            mb="3"
+            me="3"
+            onClick={onAddIncomeOpen}
+          >
+            <AddIcon me="3" />
+            Add Income
+          </Button>
+          <Button
+            minWidth="160"
+            colorScheme="red"
+            mb="3"
+            onClick={onAddExpenseOpen}
+          >
+            <MinusIcon me="3" />
+            Add Expense
+          </Button>
+        </Flex>
+        <Button minWidth="160" onClick={handleBack}>
+          Go back
         </Button>
-        <Button onClick={handleBack}>Go back</Button>
       </Flex>
     </Flex>
   );
